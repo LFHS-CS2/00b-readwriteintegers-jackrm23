258ddc0cbd4fabc045619c90322918a9eced1e82
jackrm23 <70001923+jackrm23@users.noreply.github.com>
2020-08-25 13:50:24 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ import java.io.*;
 import java.util.*;
 class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        Scanner in = new Scanner(new File("test.in"));
+    PrintWriter out = new PrintWriter("test.out");
+    while (in.hasNextInt()) {
+      out.println(in.nextInt()+1);
+    }
+    in.close();
+    out.close();
 
     }
 }
